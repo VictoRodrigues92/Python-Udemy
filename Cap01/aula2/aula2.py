df80ea3957517f8081fe04bcccb1eb4a72343d84
João Victor <52130373+VictoRodrigues92@users.noreply.github.com>
2022-03-08 20:50:54 -0300

    Add files via upload

---
+++ Cap01/aula2/aula2.py
@@ -0,0 +1,9 @@
+print('João','Victor', sep='-', end='$$$$\n')
+print('João', 'e', 'Maria', sep='-', end='####\n')
+print('executar')
+
+#EXERCICIO SEPARE O CPF COM QUALQUER CARACTER
+
+print('824', '.176', '.070', '-18', sep='x') #eu fiz
+print('824', '176', '070', sep='.', end='-')#prof fez
+print('18')
---
+++ Cap01/aula6/aula6-2.py
@@ -0,0 +1,13 @@
+nome = 'João Victor'
+idade = 29
+altura = 1.75
+e_maior = idade > 18
+peso = 80
+imc = peso / (altura ** 2)
+
+print('Nome:', nome)
+print('Idade:', idade)
+print('Altura:', altura)
+print('É maior:', e_maior)
+
+print(nome, 'tem', idade, 'anos de idade e seu imc é ', imc)
